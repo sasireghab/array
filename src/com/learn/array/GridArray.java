55f40472e65c0c5b06b7fd66dc91a59719f3396c
Administrator <Administrator@User-PC>
2017-11-05 16:04:09 +0000

    commit the project

---
+++ src/com/learn/array/GridArray.java
@@ -0,0 +1,26 @@
+package com.learn.array;
+
+public class GridArray {
+
+	public static void main(String[] args) {
+
+		
+		int[][] mark = {
+				{76,86,85,98,77},
+				{98,87,65,79,85},
+				{67,95,93,72,81}
+			};
+		System.out.println("ARRAY LENGHT ARE :: "+mark.length);
+		int len = mark.length;
+		int len1 = mark[0].length;
+
+		for( int i = 0 ; i < len ; i++)
+		{
+			for( int j = 0 ; j < len1; j++)
+			{
+				System.out.println(" ARRAY VALUES ARE :: "+mark[i][j]);
+			}
+		}
+	}
+
+}
